d40395f4154c1fba2a8fa6415b84b0dd602c0e87
SaadNabeel <saadnabeel2010@gmail.com>
2024-09-14 15:04:45 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mult(a,b):
+    return a*b
+def divi(a,b):
+    return a/b
+x=int(input("Enter the first number:"))
+y=int(input("Enter the second number:"))
+print("Addition is: ", add(x,y))
+print("Subtraction is: ", sub(x,y))
+print("Multiplication is: ", mult(x,y))
+print("Division is: ", divi(x,y))
